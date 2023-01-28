86a28cc9a7c32dcce63e5988c3a3dd83da149685
Eugene Istomin <74331980+EugeneIstomin@users.noreply.github.com>
2023-01-29 02:09:26 +0300

    Update update_manager_v2.rb

--- lib/tdlib/update_manager_v2.rb
+++ lib/tdlib/update_manager_v2.rb
@@ -29,19 +29,22 @@ class TD::UpdateManagerV2
     updateNotifier = ['updatePoll', 'updateUser', 'updateUsersNearby', 'updateUserFullInfo' 'updateSupergroup', 'updateSupergroupFullInfo', 'updateNewChat', 'updateChatLastMessage', 'updateNewMessage', 'updateUserStatus', 'updateDeleteMessages', 'updateChatAction', 'updateMessageInteractionInfo', 'updateBasicGroup', 'updateMessageContent', 'updateMessageEdited', 'updateBasicGroupFullInfo', 'updateChatMember', 'updateChatMessageSender', 'updateChatMessageTtl', 'updateChatNotificationSettings', 'updateChatOnlineMemberCount', 'updateChatPendingJoinRequests', 'updateChatPermissions', 'updateChatPhoto', 'updateMessageSendAcknowledged', 'updateMessageSendSucceeded', 'updateNewCallbackQuery', 'updateNewChatJoinRequest', 'updateNewChosenInlineResult', 'updateNewCustomEvent', 'updateNewCustomQuery', 'updateNewInlineCallbackQuery', 'updateNewInlineQuery', 'updateNewPreCheckoutQuery', 'updateNewShippingQuery', 'updatePollAnswer', 'updateServiceNotification', 'updateCall', 'updateAnimatedEmojiMessageClicked']
     
     update = TD::ApiV2.client_receive(TIMEOUT)
-    if  updateNotifier.include?(update['@type'])
-      #
-      data = update.to_h
-      #
-      meta = {}
-      meta[:type]   = :update
-      meta[:profile]   = profile
-      meta[:timestamp] = DateTime.now.strftime('%Q').to_i
-      meta[:tdType]    = data['@type']
-      meta[:dataThumb] = Digest::SHA1.hexdigest(update.to_s)
-      meta[:uuid]      = Digest::SHA1.hexdigest("#{meta[:timestamp]}::#{meta[:type]}::#{meta[:profile]}::#{meta[:tdType]}::#{meta[:dataThumb]}")          
-      #
-      bus.send(meta: meta, data: data)
+    begin
+      if update.class == Hash && updateNotifier.include?(update['@type'])
+        data = update.to_h
+        #
+        meta = {}
+        meta[:type]   = :update
+        meta[:profile]   = profile
+        meta[:timestamp] = DateTime.now.strftime('%Q').to_i
+        meta[:tdType]    = data['@type']
+        meta[:dataThumb] = Digest::SHA1.hexdigest(update.to_s)
+        meta[:uuid]      = Digest::SHA1.hexdigest("#{meta[:timestamp]}::#{meta[:type]}::#{meta[:profile]}::#{meta[:tdType]}::#{meta[:dataThumb]}")          
+        #
+        bus.send(meta: meta, data: data)
+      end
+    rescue => e
+      p "TDLIB SEND UPDATE ERROR: #{e}"
     end
 
     unless update.nil?
